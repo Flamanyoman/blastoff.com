2768e3c34462b2ab0e1a3be821930c0aa3116dc7
DPMK48 <kadaladorathy@gmail.com>
2023-09-05 14:53:45 +0100

    final commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs')
+
+app.listen(3000, ()=> {
+    console.log('app listening')
+});
+
+app.use(express.static('./public'));
+
+const errorHandler = (error, req, res, next) => {
+    if (error) {
+        res.render('404');
+        console.log(error)
+    }
+}
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.status(200).render('home', {title: "Home"});
+});
+
+app.get('/library', (req, res) => {
+    res.status(200).render('library', {title: "Library"})
+})
+
+app.use((req, res) => {
+    res.status(404).render("404", { title: "404" });
+  });
+
+
+//   ERROR HANDLER MIDDLEWARE
+app.use(errorHandler)
+
